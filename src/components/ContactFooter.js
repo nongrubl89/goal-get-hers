c27b3da4967ba419b21a56e2ca192f1f8287069c
Lisa <lisaburgnon@gmail.com>
2021-04-05 13:25:15 -0700

    added logos to footer

--- src/components/ContactFooter.js
+++ src/components/ContactFooter.js
@@ -1,19 +1,22 @@
 import React from "react";
+import Logo from "../OrangeLogo.png";
 
 export default function ContactFooter() {
   return (
     <div>
       <footer className="my-5 pt-5 text-muted text-center text-small">
-        <p className="mb-1">Find Us Here:</p>
+        <div className="mb-1 text-center">
+          <img className="logo-in-footer" src={Logo} alt="logo"></img>
+        </div>
         <ul className="list-inline">
           <li className="list-inline-item">
-            <p>Insta Logo Here</p>
+            <i class="bi bi-facebook"></i>
           </li>
           <li className="list-inline-item">
-            <p>FB Logo Here</p>
+            <i class="bi bi-instagram"></i>
           </li>
           <li className="list-inline-item">
-            <p>Email Logo Here</p>
+            <i class="bi bi-twitter"></i>
           </li>
         </ul>
       </footer>
--- src/components/HomeCard.js
+++ src/components/HomeCard.js
@@ -1,4 +1,4 @@
-import { Card, Button } from "react-bootstrap";
+import { Card } from "react-bootstrap";
 import React from "react";
 
 export default function HomeCard(props) {
